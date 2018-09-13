175cd0b36d242514a2b28817773d47bf2cab0ce9
Mihaela Petkova <mishippp@gmail.com>
2018-09-13 15:03:30 +0300

    Servo and potentiometer

--- TestServo/TestServo/TestServo.ino
+++ TestServo/TestServo/TestServo.ino
@@ -6,30 +6,36 @@
 
 #include <Servo.h>
 
-Servo myservo;  
-
-int pos = 0;    // variable to store the servo position
-float Vcc = 0;	// variable to Vcc
+Servo myServo;  
+int const potPin = A0;  // analog pin used to connect the potentiometer
+int potVal;				// variable to read the value from the analog pin
+int angle;				// variable to hold the angle for the servo motor
 
 void setup() 
 {
-	myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+	myServo.attach(9);                 // attaches the servo on pin 9 to the servo object
 	Serial.begin(9600);
 }
 
 void loop() 
 {
-	for (pos = 0; pos <= 180; pos += 1) 
-	{ // goes from 0 degrees to 180 degrees
-										  // in steps of 1 degree
-		myservo.write(pos);              // tell servo to go to position in variable 'pos'
-		delay(15);                       // waits 15ms for the servo to reach the position
-	}
-	for (pos = 180; pos >= 0; pos -= 1)
-	{ // goes from 180 degrees to 0 degrees
-		myservo.write(pos);              // tell servo to go to position in variable 'pos'
-		delay(15);                       // waits 15ms for the servo to reach the position
-	}
+	potVal = analogRead(potPin); // read the value of the potentiometer
+								 // print out the value to the Serial Monitor
+	Serial.print("potVal: ");
+	Serial.print(potVal);
+
+	// scale the numbers from the pot
+	angle = map(potVal, 0, 1023, 0, 179);
+
+	// print out the angle for the servo motor
+	Serial.print(", angle: ");
+	Serial.println(angle);
+
+	// set the servo position
+	myServo.write(angle);
+
+	// wait for the servo to get there
+	delay(10);
 
 	ReadSensor();
 }
@@ -37,10 +43,15 @@ void loop()
 void ReadSensor()
 {
   float value = 0;
+
   for(int i = 0; i < 1000; i++) 
   {
     value = ((analogRead(A0)-512)*20*2.5)/1023;
     delay(1);
   }
+
+  Serial.print("Amp: ");
   Serial.println(value);  
+
+  delay(10);
 }
