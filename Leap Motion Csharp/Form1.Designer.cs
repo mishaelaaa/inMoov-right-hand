781208d559e7b930c13cecff3f47aaa2a59988b9
Mihaela Petkova <mishippp@gmail.com>
2018-09-15 22:40:48 +0300

    Coordinates

--- Leap Motion Csharp/Form1.Designer.cs
+++ Leap Motion Csharp/Form1.Designer.cs
@@ -21,20 +21,213 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.displayImages = new System.Windows.Forms.PictureBox();
+            this.FHand = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox6 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox7 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox8 = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.displayImages)).BeginInit();
             this.SuspendLayout();
+            // 
+            // displayImages
+            // 
+            this.displayImages.Location = new System.Drawing.Point(137, 3);
+            this.displayImages.Margin = new System.Windows.Forms.Padding(4);
+            this.displayImages.Name = "displayImages";
+            this.displayImages.Size = new System.Drawing.Size(756, 543);
+            this.displayImages.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.displayImages.TabIndex = 4;
+            this.displayImages.TabStop = false;
+            // 
+            // FHand
+            // 
+            this.FHand.AutoSize = true;
+            this.FHand.Location = new System.Drawing.Point(0, 12);
+            this.FHand.Name = "FHand";
+            this.FHand.Size = new System.Drawing.Size(46, 17);
+            this.FHand.TabIndex = 5;
+            this.FHand.Text = "X-Pos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(0, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(0, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "label8";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(52, 12);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox1.TabIndex = 13;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(53, 45);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox2.TabIndex = 14;
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(52, 275);
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox3.TabIndex = 15;
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(52, 81);
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox4.TabIndex = 16;
+            // 
+            // maskedTextBox5
+            // 
+            this.maskedTextBox5.Location = new System.Drawing.Point(52, 236);
+            this.maskedTextBox5.Name = "maskedTextBox5";
+            this.maskedTextBox5.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox5.TabIndex = 17;
+            // 
+            // maskedTextBox6
+            // 
+            this.maskedTextBox6.Location = new System.Drawing.Point(52, 198);
+            this.maskedTextBox6.Name = "maskedTextBox6";
+            this.maskedTextBox6.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox6.TabIndex = 18;
+            // 
+            // maskedTextBox7
+            // 
+            this.maskedTextBox7.Location = new System.Drawing.Point(52, 157);
+            this.maskedTextBox7.Name = "maskedTextBox7";
+            this.maskedTextBox7.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox7.TabIndex = 19;
+            // 
+            // maskedTextBox8
+            // 
+            this.maskedTextBox8.Location = new System.Drawing.Point(52, 119);
+            this.maskedTextBox8.Name = "maskedTextBox8";
+            this.maskedTextBox8.Size = new System.Drawing.Size(78, 22);
+            this.maskedTextBox8.TabIndex = 20;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 362);
+            this.ClientSize = new System.Drawing.Size(893, 547);
+            this.Controls.Add(this.maskedTextBox8);
+            this.Controls.Add(this.maskedTextBox7);
+            this.Controls.Add(this.maskedTextBox6);
+            this.Controls.Add(this.maskedTextBox5);
+            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FHand);
+            this.Controls.Add(this.displayImages);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Frame Data";
+            ((System.ComponentModel.ISupportInitialize)(this.displayImages)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox displayImages;
+        private System.Windows.Forms.Label FHand;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox6;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox7;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox8;
     }
 }
--- Leap Motion Csharp/Form1.cs
+++ Leap Motion Csharp/Form1.cs
@@ -13,65 +13,136 @@ using Leap;
 
 namespace Leap_Motion_Csharp
 {
-    public partial class Form1 : Form//, ILeapEventDelegate
+    public partial class Form1 : Form
     {
-        private Controller controller;
-        private LeapEventListener listener;
+        private byte[] imagedata = new byte[1];
+        private Controller controller = new Controller();
+        Bitmap bitmap = new Bitmap(640, 480, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
 
         public Form1()
         {
             InitializeComponent();
+            
+            controller.EventContext = WindowsFormsSynchronizationContext.Current;
+            controller.FrameReady += newFrameHandler;
+            controller.ImageReady += onImageReady;
+            controller.ImageRequestFailed += onImageRequestFailed;
 
-            this.controller = new Controller();
-            this.listener = new LeapEventListener(this);
-            SampleListener listener;
-            controller.addListener(listener);
+            ColorPalette grayscale = bitmap.Palette;
+            for (int i = 0; i < 256; i++)
+            {
+                grayscale.Entries[i] = Color.FromArgb((int)255, i, i, i);
+            }
+            bitmap.Palette = grayscale;
         }
 
-        delegate void LeapEventDelegate(string EventName);
-        public void LeapEventNotification(string EventName)
+        double FHandX = 0, FHandZ = 0, manitute = 0, angle = 0, ServoМiddle = 0, ServoRing = 0;
+
+        int i = 0;
+
+        //Start the Frame
+        void newFrameHandler(object sender, FrameEventArgs eventArgs)
         {
-            if (!this.InvokeRequired)
+            Frame frame = eventArgs.frame;
+            //The following are Label controls added in design view for the form
+            this.displayFPS.Text = frame.CurrentFramesPerSecond.ToString();
+            this.displayHandCount.Text = frame.Hands.Count.ToString();
+            controller.RequestImages(frame.Id, Leap.Image.ImageType.DEFAULT, imagedata);
+
+            List<Hand> hands = frame.Hands;
+            if (hands != null && hands.Count >= 1)
             {
-                switch (EventName)
-                {
-                    case "onInit":
-                        MessageBox.Show("Initialized");
-                        break;
+                FHandX = hands[0].PalmPosition.x;
+                FHandZ = hands[0].PalmPosition.z;
 
-                    case "onConnect":
-                        MessageBox.Show("Leap connected");
-                        break;
-
-                    case "onFrame":
-                        MessageBox.Show("Frame taken");
-                        break;
-                }
+                manitute = Math.Sqrt(Math.Pow(FHandX, 2) + Math.Pow(FHandZ, 2));
+                // C= Sqrt(A^2 + B^2)
+                if (manitute < 40 || manitute > 240) //centerr
+                    manitute = 0;
+                else
+                    manitute = ((manitute - 40) / 200) * 100; // change the sens of the mottors 
             }
             else
             {
-                BeginInvoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+                FHandX = 0;
+                FHandZ = 0;
+                manitute = 0;
+            }
+
+            if (FHandX >= 0 && FHandZ >= 0)
+            {
+                angle = (Math.Atan(FHandZ / FHandX) * 180) / Math.PI;
+                if (angle >= 45)
+                {
+                    ServoМiddle = 90;
+                    ServoМiddle = angle;
+                }
+                else if (angle < 45)
+                {
+                    ServoМiddle = 45 + angle;
+                    ServoМiddle = angle;
+                }
+            }
+
+            else if (FHandX < 0 && FHandZ >= 0)
+            {
+                angle = (Math.Atan(FHandZ / FHandX) * 180) / Math.PI;
+                if (angle >= 45)
+                {
+                    ServoRing = 90;
+                    ServoRing = angle;
+                }
+                else if (angle < 45)
+                {
+                    ServoRing = 45 + angle;
+                    ServoRing = angle;
+                }
+            }
+            ServoМiddle = (ServoМiddle * manitute) / 90;
+            ServoRing = (ServoRing * manitute) / 90;
+
+            if (frame.Hands.Count == 0)
+            {
+                ServoRing = 0;
+                ServoМiddle = 0;
+            }
+
+            if (i < 3) i++;
+
+            else
+            {
+                ServoRing.Clear();
+                ServoМiddle.Clear();
+                Zpos.Clear();
+                Xpos.Clear();
+                ServoRing.AppendText("\n" + ServoRing.ToString("#.##"));
+                ServoМiddle.AppendText("\n" + ServoМiddle.ToString("#.##"));
+                Zpos.AppendText("\n" + FHandZ.ToString("#.##"));
+                Xpos.AppendText("\n" + FHandX.ToString("#.##"));
+                mag.AppendText("\n" + manitute.ToString("#.##"));
+                Ang.AppendText("\n" + angle.ToString("#.##"));
+                i = 0;
             }
         }
-    }
 
-    public class LeapEventListener : ConsoleTraceListener
-    {
-        public LeapEventListener(Form1 form1){ }
+ 
+        void onImageRequestFailed(object sender, ImageRequestFailedEventArgs e)
+        {
+            if (e.reason == Leap.Image.RequestFailureReason.Insufficient_Buffer)
+            {
+                imagedata = new byte[e.requiredBufferSize];
+            }
+            Console.WriteLine("Image request failed: " + e.message);
+        }
 
-        readonly ILeapEventDelegate eventDelegate;
-
-        public LeapEventListener(ILeapEventDelegate delegateObject) => this.eventDelegate = delegateObject;
-        
-        public override void OnInit(Controller controller) => this.eventDelegate.LeapEventNotiofination("onInit");
-
-        public override void OnConnect(Controller controller) => this.eventDelegate.LeapEventNotiofination("OnConnect");
-
-        public override void OnFrame(Controller controller) => this.eventDelegate.LeapEventNotiofination("OnFrame");
-
-        public override void OnExit(Controller controller) => this.eventDelegate.LeapEventNotiofination("OnExit");
-
-        public override void OnDisconnect(Controller controller) => this.eventDelegate.LeapEventNotiofination("OnDisconnect");
+        void onImageReady(object sender, ImageEventArgs e)
+        {
+            Rectangle lockArea = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            BitmapData bitmapData = bitmap.LockBits(lockArea, ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
+            byte[] rawImageData = imagedata;
+            System.Runtime.InteropServices.Marshal.Copy(rawImageData, 0, bitmapData.Scan0, e.image.Width * e.image.Height * 2 * e.image.BytesPerPixel);
+            bitmap.UnlockBits(bitmapData);
+            displayImages.Image = bitmap;
+        }
     }
 }
-
