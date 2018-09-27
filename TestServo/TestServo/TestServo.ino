bd6ca036e86c669a3e31f744340f9cb97d4c3097
Mihaela Petkova <mishippp@gmail.com>
2018-09-27 21:47:16 +0300

    Test servo

--- TestServo/TestServo/TestServo.ino
+++ TestServo/TestServo/TestServo.ino
@@ -5,15 +5,32 @@
 */
 
 #include <Servo.h>
-
-Servo myServo;  
+Servo myServoTest;
+Servo myServoTHUMB;  
+Servo myServoINDEX;  
+Servo myServoMIDDLE;  
+Servo myServoRING;
+Servo myServoPINKY;
+  
 int const potPin = A0;  // analog pin used to connect the potentiometer
 int potVal;				// variable to read the value from the analog pin
-int angle;				// variable to hold the angle for the servo motor
+
+int angleTest;
+int angleTHUMB;				// variable to hold the angle for the servo motor
+int angleINDEX;       
+int angleMIDDLE;      
+int angleRING;       
+int anglePINKY;       
 
 void setup() 
 {
-	myServo.attach(9);                 // attaches the servo on pin 9 to the servo object
+	myServoTHUMB.attach(13);                 // attaches the servo on pin 9 to the servo object
+  myServoINDEX.attach(12);  
+  myServoMIDDLE.attach(11); 
+  myServoRING.attach(10);
+  myServoPINKY.attach(9);
+  myServoTest.attach(8);
+	
 	Serial.begin(9600);
 }
 
@@ -22,22 +39,47 @@ void loop()
 	potVal = analogRead(potPin); // read the value of the potentiometer
 								               // print out the value to the Serial Monitor
 	Serial.print("potVal: ");
-	Serial.print(potVal);
+	Serial.println(potVal);
 
 	// scale the numbers from the pot
-	angle = map(potVal, 0, 1023, 0, 179);
+	//angleTHUMB = map(potVal, 0, 760, 0, 290);
+  //angleINDEX = map(potVal, 0, 780, 0, 179);
+  //angleMIDDLE = map(potVal, 0, 905, 0, 355);
+  //angleRING = map(potVal, 0, 865, 0, 300);
+  //anglePINKY = map(potVal, 0, 937, 0, 320);
+  angleTest = map(potVal, 0, 760, 0, 290);
 
 	// print out the angle for the servo motor
-	Serial.print(", angle: ");
-	Serial.println(angle);
+	//Serial.print("angleTHUMB: ");
+	//Serial.println(angleTHUMB);
+
+  //Serial.print("angleINDEX: ");
+  //Serial.println(angleINDEX);
+
+  //Serial.print("angleMIDDLE: ");
+  //Serial.println(angleMIDDLE);
+
+  //Serial.print("angleRING: ");
+  //Serial.println(angleRING);
+
+  //Serial.print("anglePINKY: ");
+  //Serial.println(anglePINKY);
+
+  Serial.print("ANGLE TEST : ");
+  Serial.println(angleTest);
 
 	// set the servo position
-	myServo.write(angle);
-
+	//myServoTHUMB.write(angleTHUMB);
+  //myServoINDEX.write(angleINDEX); 
+  //myServoMIDDLE.write(angleMIDDLE);  
+  //myServoRING.write(angleRING);
+  //myServoPINKY.write(anglePINKY);
+  myServoTest.write(angleTest);
+  
 	// wait for the servo to get there
-	delay(10);
+	delay(1);
 
-	ReadSensor();
+	//ReadSensor();
 }
 
 void ReadSensor()
@@ -45,9 +87,10 @@ void ReadSensor()
   float value = 0;
 
   for(int i = 0; i < 1000; i++) 
-  {
+  { 
+    //DC Current Sensor ACS712 20V
     value = ((analogRead(A1)-512)*20*2.5)/1023;
-    delay(1);
+    delay(10);
   }
 
   Serial.print("Amp: ");
